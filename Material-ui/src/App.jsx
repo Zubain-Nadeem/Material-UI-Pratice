14315f548a52df380119da47d0b4588d8238680b
ZUBAIN NADEEM <zubainali23@gmail.com>
2026-01-10 18:49:38 +0500

    File Updated

--- Material-ui/src/App.jsx
+++ Material-ui/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MuiTypography from './Components/MuiTypography'
 import MuiButton from './Components/MuiButton'
 import MuiTextField from './Components/MuiTextField'
-import MuiSelect from './Components/MuiSelect'
+import MuiSelect from './Components/MuiSelect';
 
 
 const App = () => {
